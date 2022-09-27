e82ae6a9a9238bc03ac02854b8a144de6a8f6c6e
Pauline Jeremie <pauline.jeremie@gmail.com>
2022-09-27 12:19:53 +0100

    Style mobile header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import "./App.css";
 import Header from "./components/header/Header";
-import Logo from "./assets/Logo/BrainFlix-logo.svg";
-import Avatar from "./assets/Images/Mohan-muruge.jpg";
-import Icon from "./assets/Icons/search.svg";
-import UploadIcon from "./assets/Icons/upload.svg";
 
 function App() {
   return (
     <>
-      <Header logo={Logo} avatar={Avatar} icon={Icon} uploadIcon={UploadIcon} />
+      <Header />
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,27 @@
-import "./Header.scss"
+import Logo from "../../assets/Logo/BrainFlix-logo.svg";
+import Avatar from "../../assets/Images/Mohan-muruge.jpg";
+import Icon from "../../assets/Icons/search.svg";
+import UploadIcon from "../../assets/Icons/upload.svg";
+import "./Header.scss";
 
-const Header = (props) => {
-    return (
-      <header className="header">
-          <img src={props.logo} alt="" className="header__logo" />
-          <div className="header__wrapper">
-            <div className="header__search">        <img src={props.icon} alt="A search icon" className="header__icon" />
-            <input className="header__input" placeholder="Search" /></div>
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={Logo} alt="" className="header__logo" />
+      <div className="header__wrapper">
+        <div className="header__search">
+          {" "}
+          <img src={Icon} alt="A search icon" className="header__icon" />
+          <input className="header__input" placeholder="Search" />
+        </div>
+        <div className="header__avatar"></div>
+      </div>
+      <a href="#" className="button">
+        <img src={UploadIcon} alt="A man" className="button__icon" />
+        <p className="button__copy">Upload</p>
+      </a>
+    </header>
+  );
+};
 
-            <img src={props.avatar} alt="A man" className="header__avatar" />
-
-            </div>
-            <a href="#" className="button">
-<img src={props.uploadIcon} alt="A man" className="header__avatar" />
-  <p className="button__copy">Upload</p>
-</a>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+export default Header;
